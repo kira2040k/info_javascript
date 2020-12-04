fa99987339a29eae07b1c9c3d399e449d1a97910
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-04 18:59:33 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,11 +1,11 @@
 //javascript code for get information from user 
-document.getElementById('useragent').innerHTML = 'user-agent :'+navigator.userAgent
-document.getElementById('platform').innerHTML = 'platform :'+navigator.platform 
-document.getElementById('language').innerHTML = 'language :'+navigator.language
+document.getElementById('useragent').innerHTML = 'user-agent :'+navigator.userAgent;
+document.getElementById('platform').innerHTML = 'platform :'+navigator.platform;
+document.getElementById('language').innerHTML = 'language :'+navigator.language;
 document.getElementById('screen').innerHTML = 'your screen : ' + screen.width + 'x' + screen.height; 
-console.log(navigator.hardwareConcurrency)
-console.log(navigator.product)
-console.log(navigator.deviceMemory)
+console.log(navigator.hardwareConcurrency);
+console.log(navigator.product);
+console.log(navigator.deviceMemory);
 let batteryPromise = navigator.getBattery();
 batteryPromise.then(batteryCallback);
 
