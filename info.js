bee9fff2cffe8562c24af2a6b9b2844e97780e1e
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-04 18:52:41 +0300

    Update info.js

--- info.js
+++ info.js
@@ -1,13 +1,9 @@
-/*
-javascript code for get information about the user
-
-
-*/
+//javascript code for get information from user 
 document.getElementById('useragent').innerHTML = 'user-agent :'+navigator.userAgent
 document.getElementById('platform').innerHTML = 'platform :'+navigator.platform 
 document.getElementById('language').innerHTML = 'language :'+navigator.language
 document.getElementById('screen').innerHTML = 'your screen : ' + screen.width + 'x' + screen.height; 
-//threads in cpu navigator.hardwareConcurrency
+console.log(navigator.hardwareConcurrency)
 console.log(navigator.product)
 console.log(navigator.deviceMemory)
 let batteryPromise = navigator.getBattery();
