17e6e703ec173c3a8c214a405999de692eede944
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-04 18:24:44 +0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,20 @@
+document.getElementById('useragent').innerHTML = 'user-agent :'+navigator.userAgent
+document.getElementById('platform').innerHTML = 'platform :'+navigator.platform 
+document.getElementById('language').innerHTML = 'language :'+navigator.language
+document.getElementById('screen').innerHTML = 'your screen : ' + screen.width + 'x' + screen.height; 
+//threads in cpu navigator.hardwareConcurrency
+console.log(navigator.product)
+console.log(navigator.deviceMemory)
+let batteryPromise = navigator.getBattery();
+batteryPromise.then(batteryCallback);
+
+function batteryCallback(batteryObject) {
+   printBatteryStatus(batteryObject);
+}
+function printBatteryStatus(batteryObject) {
+    console.log("IsCharging", batteryObject.charging);
+    console.log("Percentage", batteryObject.level);
+   
+    console.log("charging Time", batteryObject.chargingTime);
+    console.log("DisCharging Time", batteryObject.dischargingTime);
+}
