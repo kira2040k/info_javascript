7cf85f0a86fbd8267df40cd8b8913dd9f0b83f08
kira2040k <46848151+kira2040k@users.noreply.github.com>
2020-12-05 16:05:07 +0300

    Update info.js

--- info.js
+++ info.js
@@ -3,7 +3,10 @@ document.getElementById('useragent').innerHTML = 'user-agent :'+navigator.userAg
 document.getElementById('platform').innerHTML = 'platform :'+navigator.platform;
 document.getElementById('language').innerHTML = 'language :'+navigator.language;
 document.getElementById('screen').innerHTML = 'your screen : ' + screen.width + 'x' + screen.height; 
-document.getElementById('CPU').innerHTML = 'CPU threads : '+navigator.hardwareConcurrency
+document.getElementById('CPU').innerHTML = 'CPU threads : '+navigator.hardwareConcurrency;
+document.getElementById('color').innerHTML = 'colorDepth :' +screen.colorDepth;
+console.log(screen.pixelDepth);
+
 function detectGPU(){
     //GPU detection
     var canvas = document.createElement('canvas');
@@ -29,10 +32,56 @@ function detectGPU(){
 }
 detectGPU();
 
+navigator.getBattery().then(function(battery) {
+  function updateAllBatteryInfo(){
+    updateChargeInfo();
+    updateLevelInfo();
+    updateChargingInfo();
+    updateDischargingInfo();
+  }
+  updateAllBatteryInfo();
 
+  battery.addEventListener('chargingchange', function(){
+    updateChargeInfo();
+  });
+  function updateChargeInfo(){
+    document.getElementById('ischarge').innerHTML= ("Battery charging? "
+                + (battery.charging ? "Yes" : "No"));
+  }
 
+  battery.addEventListener('levelchange', function(){
+    updateLevelInfo();
+  });
+  function updateLevelInfo(){
+    document.getElementById('level').innerHTML = ("Battery level: "
+                + battery.level * 100 + "%");
+  }
 
+  battery.addEventListener('chargingtimechange', function(){
+    updateChargingInfo();
+  });
+  function updateChargingInfo(){
+    document.getElementById('charging').innerHTML= ("Battery charging time: "
+                 + battery.chargingTime + " seconds");
+  }
 
+  battery.addEventListener('dischargingtimechange', function(){
+    updateDischargingInfo();
+  });
+  function updateDischargingInfo(){
+    document.getElementById('discharging').innerHTML = ("Battery discharging time: "
+                 + battery.dischargingTime + " seconds");
+  }
 
+});
+if(screen.width < 500 ||
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPod/i)) {
+    document.getElementById('ismobile').innerHTML =("This is a mobile device");
+ }
+ else{
+  document.getElementById('ismobile').innerHTML =("using not a mobile")
 
-
+ }
